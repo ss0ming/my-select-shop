d49f1bc8571965acc313fce20fb18ced677817ff
ss0ming <hee9920@naver.com>
2025-02-04 19:45:02 +0900

    feat: 상품 페이징 및 정렬 구현

--- src/main/java/com/ss0ming/myselectshop/service/ProductService.java
+++ src/main/java/com/ss0ming/myselectshop/service/ProductService.java
@@ -5,9 +5,14 @@ import com.ss0ming.myselectshop.dto.ProductRequestDto;
 import com.ss0ming.myselectshop.dto.ProductResponseDto;
 import com.ss0ming.myselectshop.entity.Product;
 import com.ss0ming.myselectshop.entity.User;
+import com.ss0ming.myselectshop.entity.UserRoleEnum;
 import com.ss0ming.myselectshop.naver.dto.ItemDto;
 import com.ss0ming.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,14 +34,26 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<ProductResponseDto> getProducts(User user) {
-        List<Product> productList = productRepository.findALlByUser(user);
-        List<ProductResponseDto> responseDtoList = new ArrayList<>();
+    public Page<ProductResponseDto> getProducts(User user,
+                                                int page, int size, String sortBy, boolean isAsc) {
+        // 페이징 처리
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
 
-        for (Product product : productList) {
-            responseDtoList.add(new ProductResponseDto(product));
+        // 사용자 권한 가져와서 ADMIN 이면 전체 조회, USER 면 본인이 추가한 부분 조회
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        Page<Product> productList;
+
+        if (userRoleEnum == UserRoleEnum.USER) {
+            // 사용자 권한이 USER 일 경우
+            productList = productRepository.findAllByUser(user, pageable);
+        } else {
+            productList = productRepository.findAll(pageable);
         }
-        return responseDtoList;
+
+        return productList.map(ProductResponseDto::new);
     }
 
     @Transactional
