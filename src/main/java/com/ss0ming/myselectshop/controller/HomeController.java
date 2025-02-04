5444d8ca04d8ddf0c88d4adb980bde2e9209a059
ss0ming <hee9920@naver.com>
2025-02-04 14:22:25 +0900

    feat: 메인 페이지 구현

---
+++ src/main/java/com/ss0ming/myselectshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ss0ming.myselectshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
