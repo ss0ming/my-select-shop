327baabae68f28a8ffc44b77d899a6cddc0f58b1
ss0ming <hee9920@naver.com>
2025-02-04 12:53:40 +0900

    Initial commit

---
+++ src/main/java/com/ss0ming/myselectshop/MyselectshopApplication.java
@@ -0,0 +1,13 @@
+package com.ss0ming.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss0ming/myselectshop/MyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss0ming.myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
